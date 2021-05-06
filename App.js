a551851afab4ba8a86b5ebd09f69562e9f319bf1
Yvan Zacharie <zachkouemegni@gmail.com>
2021-05-05 23:43:27 -0300

    firstCommit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,Image  } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Asset } from 'expo-asset';
+import  AppLoading  from 'expo-app-loading';
+import MusicApp from './app/index';
+
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
+ class App extends Component{
+
+    constructor(){
+      super()
+      this.state = {
+        isReady:false
+      };
+   
+    }
+
+    async _loadAssetsAsync() {
+      const imageAssets = cacheImages([require('./assets/bg.jpg')]);
+  
+      await Promise.all([...imageAssets]);
+    }
+
+      render() {
+        if (!this.state.isReady) {
+          return (
+            <AppLoading
+              startAsync={this._loadAssetsAsync}
+              onFinish={() => this.setState({ isReady: true })}
+              onError={console.warn}
+            />
+          );
+        }
+        return <MusicApp/>
+      }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App;
